e31719556b497315dc7ae445f27c0ce7166cabab
daves22 <davisilvamatias@gmail.com>
2022-12-26 12:10:34 -0300

    adicionando dados de item, jogador e loja

---
+++ docs/entregas/modulo_2/dml.sql
@@ -0,0 +1,21 @@
+/* linguagem de manipulação de dados */
+
+INSERT INTO item (idItem, descricao, usado, tipo)
+VALUES
+(1,'descricao1',100,'especificação1'),
+
+INSERT INTO jogador(idPersonagem, nome, pontosVida, experiencia)
+VALUES
+(1,'Jack', 100,100),
+(2, 'Bob', 100,100),
+(3, 'Samanta', 100,100);
+
+INSERT INTO loja(nome, descricao, estoque)
+VALUES
+('armas1','loja de armas','AK-47', 'AM-1','P90','P40','ROCKET')
+('suprimentos1','loja de consumíveis', 'suco Dollyy(cura)','Tang (aumenta o dano)')
+
+
+
+
+
