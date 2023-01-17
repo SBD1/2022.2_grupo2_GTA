ffce816527bfe17f8e88c26d6f27baf45d1c5507
@Lorenzo7377 <54644579+lorenzo7377@users.noreply.github.com>
2023-01-16 23:15:45 -0300

    Corrigido o executável

--- SQL/Move.py
+++ SQL/Move.py
@@ -1,104 +1,42 @@
-entrada = 1
 import psycopg2
-con = psycopg2.connect(host='localhost', database='Teste9',
-user='postgres', password='Thiagoareu1')
-cur = con.cursor()
-sql = 'SELECT * FROM Area'
-cur.execute(sql)
-con.commit()
-recset = cur.fetchall()
-
-for rec in recset:
-    print (rec)
 
 
+con = psycopg2.connect(
+        host='localhost',
+        dbname='gta',
+        user='postgres',
+        password='postgres'
+    )
 
-cur = con.cursor()
-cur.execute('SELECT idArea FROM jogador')
-con.commit()
-recset = cur.fetchone()
+print('Bem vindo a GTA! VC esta jogando com Jogador 1\n\n')
+with con:
+    v = 1
+    while v != 0:
+        cursor = con.cursor()
+        cursor.execute('SELECT idArea FROM Jogador WHERE idJogador=%s', [1])
+        resultados = cursor.fetchone()
+        areajogador = resultados[0]
+        cursor.execute('SELECT descricao FROM area WHERE idArea=%s', [areajogador])
+        areaAtual = cursor.fetchone()
+        
+        print('Voce esta em:')
+        print(areaAtual[0])
+        print('\nPara onde ir?\n')
 
-for rec in recset:
-    idArea_Jogador = rec
+        cursor.execute('SELECT idA2 FROM AreaCArea WHERE idA1=%s', [areajogador])
+        areas = cursor.fetchall()
+        var = len(areas)
+        cont = 1
+        for idA2 in areas:
+            cursor.execute('SELECT descricao FROM Area WHERE idArea=%s', [idA2])
+            resp = cursor.fetchone()
+            id = idA2[0]
+            descricao = resp[0]
+            print(f'{id} - {descricao}')
 
-print ('\n') 
-
-print(idArea_Jogador," = idArea")
-
-print ('\n')
-
-cur = con.cursor()
-cur.execute(f'SELECT norte, sul, leste, oeste FROM Area WHERE idArea = {idArea_Jogador};')
-con.commit()
-recset = cur.fetchall()
-
-for rec in recset:
-    print (rec)
-
-while entrada != 0:
-
-    novo_local = idArea_Jogador
-    print ('Escolha um numero referente ao lugar para qual você deseja se locomover!')
-    print ('NORTE = 1') 
-    print ('SUL = 2') 
-    print ('LESTE = 3') 
-    print ('OESTE = 4')
-    print ('SAIR = 5')
-    entrada = input ('Para onde voce dexeja se locomover?') 
-
-    if entrada == '1':
-        novo_local = novo_local - 3
-        cur = con.cursor()
-        cur.execute(f'UPDATE Jogador SET idArea = {novo_local};')
-        con.commit()
-    elif entrada == '2':
-        novo_local = novo_local + 3
-        cur = con.cursor()
-        cur.execute(f'UPDATE Jogador SET idArea = {novo_local};')
-        con.commit()
-    elif entrada == '3':
-        novo_local = novo_local + 1
-        cur = con.cursor()
-        cur.execute(f'UPDATE Jogador SET idArea = {novo_local};')
-        con.commit()
-    elif entrada == '4':
-        novo_local = novo_local - 1
-        cur = con.cursor()
-        cur.execute(f'UPDATE Jogador SET idArea = {novo_local};')
-        con.commit()
-    elif entrada == '5':
-        print("Você saiu do programa!!!")
-        con.close()
-        exit()
-
-
-    cur = con.cursor()
-    cur.execute(f'UPDATE Jogador SET idArea = {novo_local};')
-    con.commit()
-
-
-    cur = con.cursor()
-    cur.execute(f'SELECT descricao FROM Area WHERE idArea = {novo_local}')
-    con.commit()
-    recset = cur.fetchone()
-
-    for rec in recset:
-        print (rec)
-
-
-    cur = con.cursor()
-    cur.execute('SELECT idArea FROM jogador ')
-    con.commit()
-    recset = cur.fetchone()
-
-    for rec in recset:
-        idArea_Jogador = rec
-
-    print ('\n') 
-
-    print(idArea_Jogador," = idArea")
-
-
-
+        novarea = input("\nDiga a area que quer ir(digite zero para sair):")
+        if novarea != '0':
+            cursor.execute('UPDATE Jogador SET idArea = %s', [novarea])
+        else:
+            v = 0
 con.close()
-
