5fee66ea5a3dc796275b32a0938e8c40374f6f08
@Lorenzo7377 <lorenzodavinci@hotmail.com>
2023-01-30 09:37:25 -0300

    Correções no arquivo de triggers

--- docs/modulo-3/trigger_stored_proceduremd3.sql
+++ docs/modulo-3/trigger_stored_proceduremd3.sql
@@ -2,18 +2,19 @@
 CREATE OR REPLACE FUNCTION verifica_posicao() 
 RETURNS trigger AS $verifica_posicao$
 BEGIN
-		 ---SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-		if new.idarea <> old.idarea then
-			PERFORM * FROM areacarea WHERE ida1 = old.idarea and ida2 = new.idarea;
-			IF not FOUND THEN
-				RAISE notice 'área não existe ou não conectada';
-				return old;
-			END IF;
-		end if;
-		return new;
+	set transaction isolation level repeatable read;
+	if old.idarea <> new.idarea then
+		PERFORM * FROM areacarea WHERE ida1 = old.idarea and ida2 = new.idarea;
+		IF not FOUND THEN
+			RAISE notice 'área não existe ou não conectada';
+			return old;
+		END IF;
+	end if;
+	return new;
 END;
 $verifica_posicao$ LANGUAGE plpgsql;
 
 CREATE Or Replace TRIGGER verifica_posicao
 BEFORE UPDATE ON Jogador	
 for each row EXECUTE PROCEDURE verifica_posicao();
+---alter procedure verifica_posicao() set default_transaction_isolation = 'repeatable read';
