463afb336dfd36cfe1c80ac7dfe52e47c5a374f0
davi matias <69313657+daves22@users.noreply.github.com>
2023-02-01 19:58:09 -0300

    Update triger missao concluida

--- docs/modulo-3/trigger_stored_proceduremd3.sql
+++ docs/modulo-3/trigger_stored_proceduremd3.sql
@@ -1,8 +1,8 @@
 --- TRIGGERS PARA TAREFA CONCLUÍDA
 CREATE OR REPLACE FUNCTION check_tarefa_cumprida() RETURNS trigger AS $check_tarefa_cumprida$
 BEGIN 
-    if new.status = 'Concluida' and old.status <> 'Concluida' and new.id_tarefa/* ou missão*/ = (select max(id_tarefa) from tarefas where id_tarefa = new.id_tarefa) then
-        UPDATE Jogador J SET experiencia = experiencia + /*qtd_exp*/ FROM tarefa t
+    if new.status = 'Concluida' and old.status <> 'Concluida' and new.id_tarefa = (select max(id_tarefa) from tarefas where id_tarefa = new.id_tarefa) then
+        UPDATE Jogador J SET experiencia = experiencia + FROM tarefa t
         WHERE J.nome_save = new.id_jogador_save and J.id_jogador = new.id_nome_jogador and t.id_tarefa = new.id_tarefa;
     end if;
     return new;
