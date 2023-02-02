2a7477169134e8cee2aca82f261f8fe962fec95d
Letícia <72623771+LeticiaAires@users.noreply.github.com>
2023-02-01 21:55:50 -0300

    Create Database.py

---
+++ game/Database.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+import os
+
+import psycopg2
+import psycopg2.extras
+
+
+@contextmanager
+def get_connection():
+    conn = psycopg2.connect(
+        host=os.getenv('DB_HOST', 'localhost'),
+        dbname=os.getenv('DB_NAME', 'GTA'),
+        user=os.getenv('DB_USER', 'jogo'),
+        password=os.getenv('DB_PASSWORD', 'senhasupersegura')
+    )
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+AS_DICT = psycopg2.extras.RealDictCursor
